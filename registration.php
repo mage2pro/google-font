242b41ed4ae9e3c1ef3b6e9b6fafa1ead78e8b11
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-09-27 21:06:14 +0300

    no message

--- registration.php
+++ registration.php
@@ -1,3 +1,3 @@
 <?php
-use Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Df_GoogleFont', __DIR__);
+use Magento\Framework\Component\ComponentRegistrar as R;
+R::register(R::MODULE, 'Df_GoogleFont', __DIR__);
