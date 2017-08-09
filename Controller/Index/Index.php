c6a39a8a480ab50d73576b95884ad739e77a8b55
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-09 16:46:50 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -7,9 +7,12 @@ use Df\GoogleFont\Font\Variant\Preview\Params;
 use Df\GoogleFont\Fonts as _Fonts;
 use Df\GoogleFont\Fonts\Sprite;
 use Df\Core\Cache;
+// 2015-12-08
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Index extends \Magento\Framework\App\Action\Action {
 	/**
 	 * 2015-12-09
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * На странице может быть сразу несколько данных элементов управления.
 	 * Возникает проблема: как синхронизировать их одновременные обращения к серверу за данными?
 	 * Проблемой это является, потому что генерация образцов шрифтов —
@@ -22,6 +25,9 @@ class Index extends \Magento\Framework\App\Action\Action {
 	 * Эта проблема была решена в серверной части применением функции @uses df_sync
 	 * @override
 	 * @see \Magento\Framework\App\Action\Action::execute()
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.0-RC1.8/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 * @return Json
 	 */
 	function execute() {
@@ -41,12 +47,10 @@ class Index extends \Magento\Framework\App\Action\Action {
 
 	/**
 	 * 2015-12-08
+	 * @used-by execute()
 	 * @return Sprite
 	 */
-	private function sprite() {
-		if (!isset($this->{__METHOD__})) {
-			$this->{__METHOD__} = Sprite::i(_Fonts::s(), Params::fromRequest());
-		}
-		return $this->{__METHOD__};
-	}
+	private function sprite() {return dfc($this, function() {return
+		Sprite::i(_Fonts::s(), Params::fromRequest())
+	;});}
 }
