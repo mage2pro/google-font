3efb4e9c6d66d61e17cfe38aa75fa8856cd7f2b0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-17 14:12:31 +0300

    1.1.2

--- Controller/Index/Preview.php
+++ Controller/Index/Preview.php
@@ -1,59 +1,35 @@
 <?php
 namespace Df\GoogleFont\Controller\Index;
-use Df\GoogleFont\Font;
-use Df\GoogleFont\Font\Variant;
 use Df\GoogleFont\Font\Variant\Preview as VariantPreview;
 // 2016-02-17
 // Иначе bin/magento setup:di:compile почему-то выдаёт сбой:
 // Cannot use Df\GoogleFont\Fonts as Fonts because the name is already in use
 // app/code/Df/Api/Controller/Google/FontPreview.php on line 6
 use Df\GoogleFont\Fonts as _Fonts;
+// 2015-11-29
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Preview extends \Df\Framework\App\Action\Image {
 	/**
 	 * 2015-11-29
 	 * @override
 	 * @see \Df\Framework\App\Action\Image::contents()
+	 * @used-by \Df\Framework\App\Action\Image::execute()
 	 * @return string
 	 */
-	protected function contents() {return $this->preview()->contents();}
+	final protected function contents() {/** @var string[] $familyA */ return
+		_Fonts::s()->get(df_first($familyA = explode(':', df_request('family'))))
+			->variant(
+				dfa($familyA, 1, 'regular') // 2015-11-29  E.g.: "regular", "italic", "700", "700italic"
+			)
+			->preview()->contents()
+	;}
 
 	/**
 	 * 2015-11-29
 	 * @override
 	 * @see \Df\Framework\App\Action\Image::type()
+	 * @used-by \Df\Framework\App\Action\Image::execute()
 	 * @return string
 	 */
-	protected function type() {return 'png';}
-
-	/** @return string */
-	private function family() {return df_first($this->familyA());}
-
-	/** @return string[] */
-	private function familyA() {
-		if (!isset($this->{__METHOD__})) {
-			$this->{__METHOD__} = explode(':', df_request('family'));
-		}
-		return $this->{__METHOD__};
-	}
-
-	/** @return Font */
-	private function font() {return _Fonts::s()->get($this->family());}
-
-	/** @return VariantPreview */
-	private function preview() {
-		if (!isset($this->{__METHOD__})) {
-			$this->{__METHOD__} = $this->variant()->preview();
-		}
-		return $this->{__METHOD__};
-	}
-
-	/** @return Variant */
-	private function variant() {return $this->font()->variant($this->variantName());}
-
-	/**
-	 * 2015-11-29
-	 * Например: "regular", "italic", "700", "700italic"
-	 * @return string
-	 */
-	private function variantName() {return dfa($this->familyA(), 1, 'regular');}
+	final protected function type() {return 'png';}
 }
