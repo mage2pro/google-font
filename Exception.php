366df2b512811832493486ee5d0c941037b1ff12
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-31 17:10:34 +0000

    refactoring

--- Exception.php
+++ Exception.php
@@ -1,25 +1,22 @@
 <?php
 namespace Df\GoogleFont;
-/**
- * 2015-11-17
- * В документации об этом ни слова не сказано,
- * однако в случае сбоя Google API возвращает JSON следующией структуры:
-	{
-		error: {
-			errors: [
-				{
-					domain: "usageLimits",
-					reason: "accessNotConfigured",
-					message: "Access Not Configured. The API (Google Fonts Developer API) is not enabled for your project. Please use the Google Developers Console to update your configuration.",
-					extendedHelp: "https://console.developers.google.com"
-				}
-			],
-			code: 403,
-			message: "Access Not Configured. The API (Google Fonts Developer API) is not enabled for your project. Please use the Google Developers Console to update your configuration."
-		}
-	}
- * https://developers.google.com/fonts/docs/developer_api
- */
+# 2015-11-17
+# В документации об этом ни слова не сказано, однако в случае сбоя Google API возвращает JSON следующией структуры:
+#	{
+#		error: {
+#			errors: [
+#				{
+#					domain: "usageLimits",
+#					reason: "accessNotConfigured",
+#					message: "Access Not Configured. The API (Google Fonts Developer API) is not enabled for your project.Please use the Google Developers Console to update your configuration.",
+#				extendedHelp: "https://console.developers.google.com"
+#				}
+#			],
+#			code: 403,
+#			message: "Access Not Configured. The API (Google Fonts Developer API) is not enabled for your project. Please use the Google Developers Console to update your configuration."
+#		}
+#	}
+# https://developers.google.com/fonts/docs/developer_api
 final class Exception extends \Df\Core\Exception {
 	/**
 	 * 2015-11-27
