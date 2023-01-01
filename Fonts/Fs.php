72dc623ac5ea361ee2e85da215861fb9b129e639
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-01 17:08:33 +0000

    https://github.com/mage2pro/core/issues/168

--- Fonts/Fs.php
+++ Fonts/Fs.php
@@ -14,9 +14,8 @@ class Fs {
 
 	/**
 	 * 2015-12-08
-	 * @return string
 	 */
-	function nameColorsSizeMargin() {return dfc($this, function() {$p = $this->params(); return
+	function nameColorsSizeMargin():string {return dfc($this, function() {$p = $this->params(); return
 		implode('_', [
 			's' . df_pad0(2, $p->fontSize())
 			,'f' . implode('-', $p->fontColor())
